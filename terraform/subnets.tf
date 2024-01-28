4b65515cf15de313888c114b920f8d885ff78eb6
ubdevops9 <132242433+ubdevops9@users.noreply.github.com>
2024-01-28 13:16:01 +0530

    Update subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -2,10 +2,16 @@ resource "aws_subnet" "public_subnets" {
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = "10.0.1.0/24"
 
+  tags = {
+    Name = "Public Subnet Netflix"
+  }
 }
 
 resource "aws_subnet" "private_subnets" {
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = "10.0.2.0/24"
 
+  tags = {
+    Name = "Private Subnet Netflix"
+  }
 }
