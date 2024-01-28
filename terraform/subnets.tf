9b1fdc707c4d696b0f301b7eb9738f23a149bf0c
ubdevops9 <132242433+ubdevops9@users.noreply.github.com>
2024-01-28 12:33:26 +0530

    Update subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,21 +1,11 @@
 resource "aws_subnet" "public_subnets" {
-  count             = length(var.public_subnet_cidrs)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = element(var.public_subnet_cidrs, count.index)
-  availability_zone = element(var.azs, count.index)
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.1.0/24"
 
-  tags = {
-    Name = "Public Subnet ${count.index + 1}"
-  }
 }
 
 resource "aws_subnet" "private_subnets" {
-  count             = length(var.private_subnet_cidrs)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = element(var.private_subnet_cidrs, count.index)
-  availability_zone = element(var.azs, count.index)
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.2.0/24"
 
-  tags = {
-    Name = "Private Subnet ${count.index + 1}"
-  }
 }
